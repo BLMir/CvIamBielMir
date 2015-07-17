4afcad613d31149a784b95fe721bd3c53f7a7615
Biel Mir <iambielmir@gmail.com>
2015-07-17 22:22:46 +0100

    first push resumeCv IamBielMir

---
+++ js/init.js
@@ -0,0 +1,8 @@
+(function($){
+  $(function(){
+
+      $('.button-collapse').sideNav();
+      $('.scrollspy').scrollSpy();
+      $('.tooltipped').tooltip({delay: 50});
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
